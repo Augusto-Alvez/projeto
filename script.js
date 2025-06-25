40a65e6007b028ea1b06ae6923d218f10ce81df1
Augusto <augusto2009alves@gmail.com>
2025-06-25 18:00:07 -0300

    corrigindo bugs e persolnalização.

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function toggleMode () {
 
   const img = document.querySelector("#profile img")
   if(html.classList.contains("light")){
-    img.setAttribute("src", "")
+    img.setAttribute("src", "./assets/2o.png")
     img.setAttribute(
       "alt",
       "Foto do Augusto, de óculos de sol e moletom azul num fundo branco."
     )
   }
   else {
-    img.setAttribute("src", "")
+    img.setAttribute("src", "./assets/1o.png")
     img.setAttribute(
       "alt",
       "Foto do Augusto, de moletom azul num fundo branco."
